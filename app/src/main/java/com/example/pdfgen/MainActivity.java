613932081f012b2e2ea455fddd304c1468ad7bc9
masum43 <masum.abir43@gmail.com>
2020-07-04 15:42:57 +0600

    ButterKnife Plugin implemented

--- app/src/main/java/com/example/pdfgen/MainActivity.java
+++ app/src/main/java/com/example/pdfgen/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.pdfgen;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button)
+    Button button1;
+    @BindView(R.id.button2)
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick({R.id.button, R.id.button2})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                break;
+            case R.id.button2:
+                break;
+        }
     }
 }
